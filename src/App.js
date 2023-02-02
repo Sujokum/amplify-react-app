7721a0a27d1d79a65a111c2a2c21c23785dcdb94
Sujokum <suneelahmed413@gmail.com>
2023-02-02 17:45:06 +0500

    asd

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <h1>Hello My Name Is Suneel Sujo </h1>
+      <h1>I'm Lerning Aws Amplify :)  </h1>
+    </div>
   )
 }
 
